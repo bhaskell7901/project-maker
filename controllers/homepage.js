5312d86f26a1e4f0c339199e104a2ef2c797720e
bhaskell7901 <bdh747flyna1@hotmail.com>
2023-02-08 13:47:56 -0800

    add update login route

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -13,3 +13,13 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/login', async (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
